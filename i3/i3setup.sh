c3a96e008606ad524db0aab8d35b2d349a7b9676
Nick Van Haver <nvhaver@gmail.com>
2016-04-16 23:02:13 +0200

    Added the inital setup for i3. To be expanded with current zsh and i3 configs.

---
+++ i3/i3setup.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# i3 setup script
+# Author: Nick Vam Haver <nvhaver@gmail.com>
+# Source: https://www.youtube.com/watch?v=j1I63wGcvU4
+
+# 0. Check if user has root privileges
+if [ "$EUID" -ne 0 ]; then 
+  echo "Please run as root" 1>&2
+  exit 1
+fi
+
+# Get distro variables
+if [ -f "/etc/arch-release" ]; then
+  DISTRIB_ID="Arch"
+else
+  . /etc/lsb-release
+fi
+
+# Prerequisites
+case $DISTRIB_ID in
+Ubuntu|Debian)
+   apt-get install i3 i3blocks lxappearance thunar gnome-icon-theme-full fonts-font-awesome
+   ;;
+Arch)
+   pacman -S i3 i3blocks lxappearance thunar gnome-icon-theme-full fonts-font-awesome
+   ;;
+*)
+   echo "Could not detect distribution to install the prerequisite packages, aborting"
+   exit
+   ;;
+esac
+
+# Generate basic config
+i3-config-wizard
+
+# Edit the config file (perhaps better copy/github?)
+i3folder=.i3
+i3conf=$i3folder"/config"
+
+cat <<EOT >> $i3conf
+
+# lock the screen
+bindsym \$mod+Shift+x exec i3lock -c 000000
+
+# Pulse Audio controls
+bindsym XF86AudioRaiseVolume exec --no-startup-id pactl set-sink-volume 0 +5% #increase sound volume
+bindsym XF86AudioLowerVolume exec --no-startup-id pactl set-sink-volume 0 -5% #decrease sound volume
+bindsym XF86AudioMute exec --no-startup-id pactl set-sink-mute 0 toggle # mute sound
+
+# Sreen brightness controls
+bindsym XF86MonBrightnessUp exec xbacklight -inc 20 # increase screen brightness
+bindsym XF86MonBrightnessDown exec xbacklight -dec 20 # decrease screen brightness
+
+# Media player controls
+bindsym XF86AudioPlay exec playerctl play
+bindsym XF86AudioPause exec playerctl pause
+bindsym XF86AudioNext exec playerctl next
+bindsym XF86AudioPrev exec playerctl previous
+EOT
+
+# Install playerctl
+playerctlSetup=/tmp/pctl.deb
+wget -O $playerctlSetup https://github.com/acrisci/playerctl/releases/download/v0.4.2/playerctl-0.4.2_amd64.deb
+dpkg -i $playerctlSetup 
+rm $playerctlSetup
+
+# Install font awesome
+awesomeZip=/tmp/awesome.zip
+wget -O $awesomeZip https://github.com/FortAwesome/Font-Awesome/archive/v4.5.0.zip
+unzip $awesomeZip
+mkdir .fonts
+mv Font-Awesome-4.5.0/fonts/fontawesome-webfont.ttf .fonts
+
+rm /tmp/awesome.zip
+rm -r Font-Awesome-4.5.0
+echo "i3wm setup complete"
